4894376033849988da88884caa9e1e6500f5fc2d
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-03-25 11:43:52 +0100

    Added a list with height conditional to the DOM

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,12 @@ let pokemonList = [
   {name:'Butterfree', height: 1.1, types: ['bug','flying']},
   {name:'Parasect', height: 1, types: ['grass','bug']}
 ];
+
+// Task 1.3 write names, heights in brackets, and add text for one specific height.
+for (let i = 0; i < pokemonList.length; i++) {
+  if (pokemonList[i].height >= 1.5) {
+  document.write(`${pokemonList[i].name} (${pokemonList[i].height}) - Wow that's big! <br>`);
+} else {
+  document.write(`${pokemonList[i].name} (${pokemonList[i].height}) <br>`);
+}
+}
