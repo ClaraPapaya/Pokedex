3def7d2037c416f3e4a2a9e35d960da39fe12970
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-03-29 10:44:51 +0200

    Added paragraph to the list items

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,8 @@ let pokemonList = [
 // Task 1.3 write names, heights in brackets, and add text for one specific height.
 for (let i = 0; i < pokemonList.length; i++) {
   if (pokemonList[i].height >= 1.5) {
-  document.write(`${pokemonList[i].name} (${pokemonList[i].height}) - Wow that's big! <br>`);
+  document.write(`<p> ${pokemonList[i].name} (${pokemonList[i].height}) - Wow that's big! <br>`);
 } else {
-  document.write(`${pokemonList[i].name} (${pokemonList[i].height}) <br>`);
+  document.write(`<p> ${pokemonList[i].name} (${pokemonList[i].height}) <br>`);
 }
 }
