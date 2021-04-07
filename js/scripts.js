a33631c559b7c0a60a10368b956b55db82a8acd0
Clara <79449522+ClaraPapaya@users.noreply.github.com>
2021-04-07 11:36:34 +0200

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,43 @@
+//IIFE
 let pokemonRepository = (function() {
+  //Empty array will be filled with Pokémon objects from an API
   let pokemonList = [];
+  //defines the API url in a variable
   let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
-
+  //returns the list of Pokémon
   function getAll() {
     return pokemonList;
   }
+  //adds a new pokemon item to the list
   function add(pokemons) {
     if (typeof pokemons === 'object' && 'name' in pokemons) {
       pokemonList.push(pokemons)
-    } else {'Not the right Pokémon!'}
+    } else {"Not the right Pokémon!"
+      }
   }
-
+  //creates an unordered list and buttons in the DOM for each Pokémon
   function addListItem(pokemon) {
     let pokemonListUl = document.querySelector('.pokemon-list');
+    //creats the list
     let listItem = document.createElement('li');
+    //creates the button
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('button-class');
     button.addEventListener('click', function(event) {
       showDetails(pokemon.name);
     });
+    //adds them to the DOM
     listItem.appendChild(button);
     pokemonListUl.appendChild(listItem);
   }
-
+  //shows the Pokémon details like type, height, etc. fetched from the details url of the API
   function showDetails(pokemon) {
     loadDetails(pokemon).then(function () {
       console.log(pokemon);
     });
   }
-
+  //loads the list of Pokémons from the API
   function loadList() {
     return fetch(apiUrl).then(function(response) {
       return response.json();
@@ -45,7 +53,7 @@ let pokemonRepository = (function() {
       console.error(e);
     })
   }
-
+  //loads the details of the Pokémons from the API
   function loadDetails(item) {
     let url = item.detailsUrl;
     return fetch(url).then(function (response) {
@@ -58,7 +66,7 @@ let pokemonRepository = (function() {
       console.error(e);
     });
   }
-
+  //key values to use the locla variables outside of the IIEF function
   return {
     getAll: getAll,
     add: add,
@@ -68,6 +76,7 @@ let pokemonRepository = (function() {
   };
 }());
 
+//calls and executes the pokemonRepository
 pokemonRepository.loadList().then(function() {
   (pokemonRepository.getAll()).forEach(function(pokemons) {
     pokemonRepository.addListItem(pokemons);
